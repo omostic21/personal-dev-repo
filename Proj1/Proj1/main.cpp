b6a748c72be6a9ecee79a0a0277c472cf80aaef2
Omolola Okesanjo <omostic21@gmail.com>
2021-06-26 01:24:32 -0500

    Added first c++ work

---
+++ Proj1/Proj1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Proj1
+//
+//  Created by Omolola Okesanjo on 6/26/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
